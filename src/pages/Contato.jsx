5c053d2bbf108d0a78b47eb64e807950351ef730
georgelucgo <georgelucgo@gmail.com>
2026-05-22 17:42:28 -0400

    Mudando icones

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,5 +1,13 @@
 import './contato.css'
 
+import {
+  FaEnvelope,
+  FaWhatsapp,
+  FaTelegramPlane,
+  FaGlobe,
+} from 'react-icons/fa'
+
+
 const Contato = () => {
   return (
     <>
@@ -45,34 +53,36 @@ const Contato = () => {
 
 
 
-      <section className="contact-options">
+       <section className="contact-options">
         <div className="container">
           <h2>Outras Formas de Contato</h2>
 
           <div className="grid">
+
             <div className="card email">
-              <span>📧</span>
+              <span><FaEnvelope /></span>
               <h3>E-mail</h3>
               <p>contato@simio.dev</p>
             </div>
 
             <div className="card whatsapp">
-              <span>💬</span>
+              <span><FaWhatsapp /></span>
               <h3>WhatsApp</h3>
               <p>(11) 99999-9999</p>
             </div>
 
             <div className="card telegram">
-              <span>📱</span>
+              <span><FaTelegramPlane /></span>
               <h3>Telegram</h3>
               <p>@simio_dev</p>
             </div>
 
             <div className="card social">
-              <span>🌐</span>
+              <span><FaGlobe /></span>
               <h3>Redes Sociais</h3>
               <p>@simiodev</p>
             </div>
+
           </div>
         </div>
       </section>
