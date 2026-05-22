5256d4c00eb488d3619f0fa121c5c4edd3ef2f67
georgelucgo <georgelucgo@gmail.com>
2026-05-22 15:55:09 -0400

    salvando alterações antes do merge

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,15 +1,6 @@
 const Contato = () => {
   return (
-    <>
-      <section className="courses-header">
-        <div className="container">
-          <div className="section-title">
-            <h2>Fale Conosco</h2>
-            <p>Tire suas dúvidas, compartilhe sugestões ou simplesmente diga oi</p>
-          </div>
-        </div>
-      </section>
-    </>
+    <></>
   );
 };
 
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,15 +1,6 @@
 const Sobre = () => {
   return (
-    <>
-      <section className="courses-header">
-        <div className="container">
-          <div className="section-title">
-            <h2>Sobre o SimioDev</h2>
-            <p>Conheça nossa história, metodologia e o que nos torna únicos no ensino de programação</p>
-          </div>
-        </div>
-      </section>
-    </>
+    <></>
   );
 };
 
