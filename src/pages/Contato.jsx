3f3837e14ead8934022fb9320976f54dae21a69a
georgelucgo <georgelucgo@gmail.com>
2026-05-22 15:35:52 -0400

    Criando contato

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,6 +1,15 @@
 const Contato = () => {
   return (
-    <></>
+    <>
+      <section className="courses-header">
+        <div className="container">
+          <div className="section-title">
+            <h2>Fale Conosco</h2>
+            <p>Tire suas dúvidas, compartilhe sugestões ou simplesmente diga oi</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
