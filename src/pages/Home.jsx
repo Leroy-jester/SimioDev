67e3686820a90c49fcc47bf9f1bbef7021201617
Leroy-jester <josefmelcher@gmail.com>
2026-05-22 20:53:53 -0400

    retirando o vídeo de teste

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <section className="hero">
         <div className="hero-content">
           <h1>Aprenda Programação do Zero ao Avançado</h1>
-          <YoutubeVideo videoId="AD7eifiEiJ8"/>
+          <YoutubeVideo videoId="AAAAAAAAAAA"/>
           <p>Domine HTML, CSS e JavaScript com projetos práticos e torne-se um desenvolvedor web completo</p>
           <div className="hero-buttons">
             <a href="/cursos" className="btn btn-primary">Começar Agora</a>
