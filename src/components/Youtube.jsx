54d05590705195df6e40da552fe949ce91e16207
Josef F Melcher <josefmelcher@gmail.com>
2026-05-26 20:01:26 -0400

    Update Youtube.jsx

--- src/components/Youtube.jsx
+++ src/components/Youtube.jsx
@@ -4,7 +4,7 @@ export function YoutubeVideo({ videoId }) {
   return (
     <div className="video-container">
       <iframe
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&playsinline=1`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
